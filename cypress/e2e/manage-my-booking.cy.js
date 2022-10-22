f5c1dd60e8a9b0c5a29825683789da116fd64253
Ha Tran <ha.tran@sastrify.com>
2022-10-22 18:01:59 +0200

    merged 2 parts together

--- cypress/e2e/manage-my-booking.cy.js
+++ cypress/e2e/manage-my-booking.cy.js
@@ -7,6 +7,8 @@ describe("Manage my booking", () => {
         const flyTo = "DXB" // Dubai
         const departAfter = "2022-11-15T00:00"
         const holdBaggage = "1"
+        const newOriginCity = "Vienna"
+        const newDestinationCity = "Barcelona"
         cy.request(
             "GET",
             `https://api.skypicker.com/flights?partner=cypress&fly_from=${flyFrom}&fly_to=${flyTo}&depart_after=${departAfter}&max_stopovers=0&adult_hold_bag=${holdBaggage}`
@@ -46,6 +48,61 @@ describe("Manage my booking", () => {
             ).then((xhr) => {
                 const reservationURL = xhr.body.mmb_link
                 cy.visit(reservationURL)
+                cy.get('[data-test="FlightsChange"]')
+                .scrollIntoView()
+                .should("be.visible")
+                .click()
+            // 3.
+            cy.contains("p", "Pay only the price difference").should("be.visible")
+            //4.
+            cy.get('[data-test="search-origin"]').first().type(newOriginCity)
+            cy.get(".ModalSimplePicker")
+                .eq(0)
+                .should("contain", newOriginCity)
+                .click()
+    
+            cy.get('[data-test="search-destination"]')
+                .first()
+                .type(newDestinationCity)
+            cy.get(".ModalSimplePicker")
+                .eq(0)
+                .should("contain", newDestinationCity)
+                .click()
+    
+            cy.get('[data-test="ChangeAlternativesSearchForm-button"]')
+                .should("be.visible")
+                .click()
+            //5.
+    
+            cy.get('[data-test="BookingJourneyChange-alternative-list"]')
+                .find('[data-test="Journey-toggle"]')
+                .eq(0)
+                .contains('[data-test="journeyInfo-price-tooltip"]', "FREE")
+                .click()
+    
+            //6.
+            cy.get('[data-test="ItineraryGuarantee"]')
+                .scrollIntoView()
+                .should("be.visible")
+            //7.
+            cy.get(
+                '[data-test="JourneyBookingButton-Itinerary-bookingBtn"]'
+            ).click()
+            //8.
+            cy.get('[data-test="changeConfirmation-NewItinerary"]').within(() => {
+                cy.contains(newDestinationCity).should("be.visible")
+                cy.contains(newOriginCity).should("be.visible")
+            })
+            cy.get('[data-test="ChangeConfirmation-CheckoutBtn"]')
+                .scrollIntoView()
+                .should("be.visible")
+                .and("be.disabled")
+    
+            cy.get('[data-test="changeConfirmation-LegalCheckbox"]')
+                .scrollIntoView()
+                .click({ force: true })
+    
+            cy.get('[data-test="ChangeConfirmation-CheckoutBtn"]').click()
             })
         })
     })
