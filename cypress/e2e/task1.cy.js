38db1d00e408253b60be54b56a09ebed82dd8a7b
norbertjanik <janik.noro@gmail.com>
2022-10-22 10:37:07 +0200

    first commit

---
+++ cypress/e2e/task1.cy.js
@@ -0,0 +1,8 @@
+describe("First task test", () => {
+    beforeEach("setup", () => {
+        cy.AllowCookies()
+        cy.CheapFlightsRootPage("/")
+    })
+
+    it("Search flight", () => {})
+})
