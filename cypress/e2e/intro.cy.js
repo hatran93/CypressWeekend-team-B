89ff1cb3eb192593315a8ae8067a139c230dcae0
vladislav775 <vlado.lesko1@gmail.com>
2022-10-22 13:08:48 +0200

    pridané príkazy

--- cypress/e2e/intro.cy.js
+++ cypress/e2e/intro.cy.js
@@ -1,6 +1,42 @@
-describe("Intro test", () => {
-    it("Check everything works", () => {
+describe("Task 1", () => {
+    it("Sheaarch One-way cheapflights ticket + one bag,", () => {
+        cy.setCookie('__kwc_agreed', 'true')
         cy.visit("/cheap-flights")
         cy.log("YAAY! IT WORKS!")
+
+        cy.get('[data-test="PictureCard"]')
+            .should('be.visible');
+
+        cy.get('[data-test="PictureCard"]').first().click();
+        cy.get('[data-test="SearchFormModesPicker-active-return"]').click();
+        cy.get('[data-test="ModePopupOption-oneWay"]').click();
+        cy.get('[data-test="PassengersField"]').click();
+
+        cy.get('[data-test="BagsPopup-cabin"]').children().eq(1).children().eq(0).children().eq(2).click();
+            cy.get('[data-test="PassengersFieldFooter-done"]')
+                .should('be.visible')
+                .click()
+
+        cy.get('[data-test="LandingSearchButton"]').click();
+            cy.get('[data-test="BagsPopup-cabin"] input')
+                .should('have.value',"1", 'be.visible');
+
+        cy.get('[data-test="SearchFormModesPicker-active-oneWay"]')
+                .should('include.text',"One-way")
+        
+        cy.get('[data-test="PlacePickerInput-origin"]')
+                .should('exist');
+        cy.get('[data-test="PlacePickerInput-origin"]')
+                .should('be.visible');
+
+        cy.get('[data-test="PlacePickerInput-destination"]')
+                .should('exist');
+        cy.get('[data-test="PlacePickerInput-destination"]')
+                .should('be.visible');
+
+        Cypress.on("uncaught:exception", (err, runnable) => {
+                    return false
+                  })        
+        
     })
 })
